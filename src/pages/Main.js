12381a9e83fbdd6a26c883a9edf99c14348ae6a4
lee <sipossa@naver.com>
2022-09-20 15:44:47 +0900

    20220920

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,38 +2,31 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Link } from 'react-router-dom';
 import '../css/Main.scss'
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useRef, useState } from 'react';
 
 const SLIDE = [
-    { id: 1, title: "01 The world expands along the new path created", content: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다", link: "/sub01" },
-    { id: 2, title: "02 The world expands along the new path created", content: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다", link: "/sub02" },
-    { id: 3, title: "03 The world expands along the new path created", content: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다", link: "/sub03" },
+    { id: 1, title: "The world expands along the new path created", content: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다", link: "/sub01" },
+    { id: 2, title: "The world expands along the new path created", content: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다", link: "/sub02" },
+    { id: 3, title: "The world expands along the new path created", content: "현대엘리베이터가 만든 새로운 길을 따라 세상은 위로 넓어집니다", link: "/sub03" },
 ]
 
-const dataurl = process.env.PUBLIC_URL + '/data/slidedata.json';
-console.log(dataurl)
-
-
-
 const Main = () => {
-    const [slide, setSlide] = useState([]);
-    const getSlideData = async () => {
-        const data = await axios.get(dataurl);
-        setSlide(data);
-    }
-    useEffect(() => {
-        getSlideData();
-        console.log(slide)
-    }, [])
+    const [num, setNum] = useState();
+    const MSG = useRef(null);
     return (
         <section className='Main'>
             <div className="mainVisual">
-                <Swiper className='mainSlider'>
+                <Swiper
+                    loop={true}
+                    //slideActiveClass={'on'}
+                    onSlideChange={it => { setNum(it.realIndex); console.log(it) }}
+                    className='mainSlider'
+                    ref={MSG}
+                >
                     {
                         SLIDE.map((slide, idx) => {
                             return (
-                                <SwiperSlide className={`itm itm${slide.id}`}>
+                                <SwiperSlide className={`itm itm0${slide.id} ${num === idx ? 'on' : ''}`} key={slide.id}>
                                     <div className="content">
                                         <p>{slide.title}</p>
                                         <div className="des">
@@ -49,6 +42,23 @@ const Main = () => {
                     }
                 </Swiper>
             </div>
+            <ul className="dots">
+                {
+                    SLIDE.map((dot, idx) => <li className={num === idx ? 'on' : ''}
+                        onClick={() => MSG.current.swiper.slideTo(idx + 1)}
+                    >{dot.id}</li>)
+                }
+            </ul>
+            <div className="tab">
+                {SLIDE[num]?.title}
+            </div>
+            <div className="slideNum">
+                {num + 1} / {SLIDE.length}
+            </div>
+            {console.log(MSG.current)}
+            <button onClick={() => MSG.current.swiper.slidePrev()}>뒤로가기</button>
+            <button onClick={() => MSG.current.swiper.slideNext()}>앞로가기</button>
+
         </section>
     )
 }
